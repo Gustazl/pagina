8288dedcbcf846f76ea84876a6ddac097d33b64d
Gustazl <gustavo.marques14@escola.pr.gov.br>
2025-08-11 22:15:53 -0300

    02.3

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Esses IDs precisam ser EXATAMENTE iguais aos IDs do seu HTML.
     const botao1 = document.getElementById('botao1');
     const botao2 = document.getElementById('botao2');
     const mensagemElemento = document.getElementById('mensagem');
 
+    // As funções de clique.
     botao1.addEventListener('click', () => {
         mensagemElemento.textContent = "Mateus viadão";
     });
