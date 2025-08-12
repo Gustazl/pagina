efbf90a998842e73751e90d43b1a60fb120b5332
Gustazl <gustavo.marques14@escola.pr.gov.br>
2025-08-11 22:04:03 -0300

    01

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const botao = document.getElementById('meuBotao');
+    const mensagem = document.getElementById('mensagem');
+
+    botao.addEventListener('click', () => {
+        mensagem.textContent = 'Olá! Você clicou no botão.';
+    });
+});
 
